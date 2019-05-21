39b57356c922686fec4a84e1ed7c81808798d41b
fdox27 <fdox27@gmail.com>
2019-05-21 23:26:17 +0200

    view with all tagged recipes

--- projekt/Controllers/TagController.cs
+++ projekt/Controllers/TagController.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projekt.Data;
+using projekt.Models;
+using projekt.Models.ViewModels;
 
 namespace projekt.Controllers
 {
     public class TagController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public TagController(AppDbContext ctx)
         {
-            return View();
+            _context = ctx;
         }
 
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public async Task<IActionResult> Index(string id)
+        {
+            Tag tag = await _context.Tags
+                .Include(t => t.Taggings)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Name == id);
+
+            TagVM tagVM = new TagVM();
+
+            if (tag != null)
+            {
+                List<Recipe> taggedRecipes = new List<Recipe>();
+                foreach (var recipe in tag.Taggings)
+                {
+                    taggedRecipes.Add(await _context.Recipes.Include(x => x.Category).FirstOrDefaultAsync(x => x.RecipeID == recipe.RecipeID));
+                }
+                tagVM.TagName = tag.Name;
+                tagVM.Recipes = taggedRecipes;
+            }
+            else
+            {
+                tagVM.TagName = id;
+                tagVM.Recipes = new List<Recipe>();
+            } 
+
+            return View(tagVM);
+        }
     }
 }
---
+++ projekt/Models/ViewModels/TagVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projekt.Models.ViewModels
+{
+    public class TagVM
+    {
+        public string TagName { get; set; }
+        public List<Recipe> Recipes { get; set; }
+    }
+}
